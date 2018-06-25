6510a9c8b1c65a7df53c49910b254313fafd43dd
Lucas Morales <lucas@lucasem.com>
2018-06-25 14:13:37 -0700

    code review: pull out special cases printing functions

--- src/pretty.rs
+++ src/pretty.rs
@@ -18,66 +18,14 @@ pub trait Pretty: Sized {
                 ("NIL", 0) | ("NULL", 0) => return "[]".to_string(),
                 (_, 0) => return op_str,
                 ("SUCC", 1) => {
-                    let mut increments = 1;
-                    let mut arg = &args[0];
-                    while let Some((op, args)) = arg.as_application() {
-                        match (op.display(sig).as_str(), args.len()) {
-                            ("SUCC", 1) => {
-                                increments += 1;
-                                arg = &args[0]
-                            }
-                            ("ZERO", 0) | ("0", 0) => return increments.to_string(),
-                            // number does not terminate with ZERO, so we use the
-                            // non-special-case printing style
-                            _ => break,
-                        }
+                    if let Some(s) = pretty_number(sig, args) {
+                        return s;
                     }
                 }
-                (".", 2) => {
-                    let mut first = &args[0];
-                    let mut rest = vec![&args[1]]; // in reverse order for fast `push`ing
-                    while let Some((op, args)) = first.as_application() {
-                        match (op.display(sig).as_str(), args.len()) {
-                            (".", 2) => {
-                                first = &args[0];
-                                rest.push(&args[1]);
-                            }
-                            _ => break,
-                        }
-                    }
-                    rest.push(first);
-                    rest.reverse();
-                    let interior = rest.into_iter()
-                        .map(|x| x.pretty_inner(sig, false))
-                        .join(" ");
-                    return if spaces_allowed {
-                        interior
-                    } else {
-                        format!("({})", interior)
-                    };
-                }
+                (".", 2) => return pretty_binary_application(sig, args, spaces_allowed),
                 ("CONS", 2) => {
-                    let mut items = vec![&args[0]];
-                    let mut cdr = &args[1];
-                    while let Some((op, args)) = cdr.as_application() {
-                        match (op.display(sig).as_str(), args.len()) {
-                            ("CONS", 2) => {
-                                items.push(&args[0]);
-                                cdr = &args[1];
-                            }
-                            ("NIL", 0) | ("NULL", 0) => {
-                                return format!(
-                                    "[{}]",
-                                    items
-                                        .into_iter()
-                                        .map(|item| item.pretty_inner(sig, true))
-                                        .join(", ")
-                                )
-                            }
-                            // list does not terminate with NIL, so we use the
-                            // non-special-case printing style
-                            _ => break,
-                        }
+                    if let Some(s) = pretty_list(sig, args) {
+                        return s;
                     }
                 }
                 _ => (),
@@ -112,4 +60,76 @@ impl Pretty for Term {
     fn display(&self, sig: &Signature) -> String {
         self.display(sig)
     }
+}
+
+fn pretty_number<T: Pretty>(sig: &Signature, args: &[T]) -> Option<String> {
+    let mut increments = 1;
+    let mut arg = &args[0];
+    while let Some((op, args)) = arg.as_application() {
+        match (op.display(sig).as_str(), args.len()) {
+            ("SUCC", 1) => {
+                increments += 1;
+                arg = &args[0]
+            }
+            ("ZERO", 0) | ("0", 0) => return Some(increments.to_string()),
+            // number does not terminate with ZERO, so we use the
+            // non-special-case printing style
+            _ => break,
+        }
+    }
+    None
+}
+
+fn pretty_binary_application<T: Pretty>(
+    sig: &Signature,
+    args: &[T],
+    spaces_allowed: bool,
+) -> String {
+    let mut first = &args[0];
+    let mut rest = vec![&args[1]]; // in reverse order for fast `push`ing
+    while let Some((op, args)) = first.as_application() {
+        match (op.display(sig).as_str(), args.len()) {
+            (".", 2) => {
+                first = &args[0];
+                rest.push(&args[1]);
+            }
+            _ => break,
+        }
+    }
+    rest.push(first);
+    rest.reverse();
+    let interior = rest.into_iter()
+        .map(|x| x.pretty_inner(sig, false))
+        .join(" ");
+    if spaces_allowed {
+        interior
+    } else {
+        format!("({})", interior)
+    }
+}
+
+fn pretty_list<T: Pretty>(sig: &Signature, args: &[T]) -> Option<String> {
+    let mut items = vec![&args[0]];
+    let mut cdr = &args[1];
+    while let Some((op, args)) = cdr.as_application() {
+        match (op.display(sig).as_str(), args.len()) {
+            ("CONS", 2) => {
+                items.push(&args[0]);
+                cdr = &args[1];
+            }
+            ("NIL", 0) | ("NULL", 0) => {
+                return Some(format!(
+                    "[{}]",
+                    items
+                        .into_iter()
+                        .map(|item| item.pretty_inner(sig, true))
+                        .join(", ")
+                ))
+            }
+            // list does not terminate with NIL, so we use the
+            // non-special-case printing style
+            _ => break,
+        }
+    }
+    None
 }
